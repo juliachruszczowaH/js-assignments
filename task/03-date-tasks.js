f45e9db8425e2e25e618b47e2150ff07ebdfdfaf
juliachruszczowaH <julia.chruszczowa@gmail.com>
2019-04-13 19:57:13 +0300

    add partial solution to 03-date-tasks.js

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,8 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   //throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 /**
@@ -37,7 +38,8 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   //throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +58,10 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   //throw new Error('Not implemented');
+   let d=new Date(date);
+   let year=d.getFullYear();
+   return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 }
 
 
@@ -77,6 +82,10 @@ function isLeapYear(date) {
  */
 function timeSpanToString(startDate, endDate) {
    throw new Error('Not implemented');
+   let diff= endDate.getMilliseconds() - startDate.getMilliseconds();
+   console.log(diff);
+   
+   return `${diff.getUTCHours()}:${diff.getMinutes()}:${diff.getSeconds()}.${diff.getMilliseconds()}`;
 }
 
 
